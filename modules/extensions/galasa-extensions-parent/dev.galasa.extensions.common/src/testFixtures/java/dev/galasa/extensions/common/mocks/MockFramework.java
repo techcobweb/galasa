38b4473d7c2c4e1da2cd9fe98ff4110fd478dc52
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-12-24 12:07:00 +0000

    extensions mock framework has rbac service
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- modules/extensions/galasa-extensions-parent/dev.galasa.extensions.common/src/testFixtures/java/dev/galasa/extensions/common/mocks/MockFramework.java
+++ modules/extensions/galasa-extensions-parent/dev.galasa.extensions.common/src/testFixtures/java/dev/galasa/extensions/common/mocks/MockFramework.java
@@ -33,6 +33,8 @@ import dev.galasa.framework.spi.auth.IAuthStore;
 import dev.galasa.framework.spi.auth.IAuthStoreService;
 import dev.galasa.framework.spi.creds.CredentialsException;
 import dev.galasa.framework.spi.creds.ICredentialsService;
+import dev.galasa.framework.spi.rbac.RBACException;
+import dev.galasa.framework.spi.rbac.RBACService;
 
 public class MockFramework implements IFramework {
 
@@ -134,6 +136,12 @@ public class MockFramework implements IFramework {
     @Override
     public @NotNull IEventsService getEventsService() {
         throw new UnsupportedOperationException("Unimplemented method 'getEventsService'");
+    }
+
+    @Override
+    public @NotNull RBACService getRBACService() throws RBACException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRBACService'");
     }
 
 }
