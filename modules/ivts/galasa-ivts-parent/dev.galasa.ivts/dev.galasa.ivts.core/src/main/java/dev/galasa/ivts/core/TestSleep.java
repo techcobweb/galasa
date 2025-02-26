a8ea2edb6ca57901928ff85f1f424e6a87bc2c25
Jade Carino <65225359+jadecarino@users.noreply.github.com>
2025-02-26 15:55:26 +0000

    Add a basic Galasa test to ivts stream that we can test  with (#209)
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

---
+++ modules/ivts/galasa-ivts-parent/dev.galasa.ivts/dev.galasa.ivts.core/src/main/java/dev/galasa/ivts/core/TestSleep.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package dev.galasa.ivts.core;
+
+import org.apache.commons.logging.Log;
+
+import dev.galasa.Summary;
+import dev.galasa.Test;
+import dev.galasa.core.manager.CoreManager;
+import dev.galasa.core.manager.ICoreManager;
+import dev.galasa.core.manager.Logger;
+import dev.galasa.core.manager.RunName;
+
+@Test
+@Summary("A basic test with a sleep so there is sufficient time to test the `galasactl runs reset` command")
+public class TestSleep {
+
+    @Logger
+    public Log logger;
+
+    @Test
+    public void sleep() throws Exception {
+        logger.info("Sleeping for 1-minute.");
+        Thread.sleep(60000);
+    }
+
+}
