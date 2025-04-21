ced71de5e38d6df8d776d6a9e49b300b652d724c
Aashir Siddiqui <aashir_sidiki@hotmail.com>
2025-04-21 18:20:54 +0100

    Added additional unit tests
    Signed-off-by: Aashir Siddiqui <aashir_sidiki@hotmail.com>

--- modules/cli/test-scripts/streams-tests.sh
+++ modules/cli/test-scripts/streams-tests.sh
@@ -114,31 +114,89 @@ function configure_stream {
     --bootstrap $bootstrap \
     --log -"
 
-    info "Command is: $cmd1"
-    info "Command is: $cmd2"
-    info "Command is: $cmd3"
-    info "Command is: $cmd4"
+    info "Adding description property: $cmd1"
+    info "Adding maven repo url property: $cmd2"
+    info "Adding test catalog url property: $cmd3"
+    info "Adding obr property: $cmd4"
 
-    $cmd1
-    $cmd2
-    $cmd3
-    $cmd4
-    local rc=$?
+    for cmd_var in cmd1 cmd2 cmd3 cmd4; do
+        # Use indirect reference to execute the command variable
+        eval "\${$cmd_var}"
+        rc=$?
+        if [[ "${rc}" != "0" ]]; then 
+            error "Failed to execute command: ${!cmd_var}"
+            return 1
+        fi
+    done
 
-    # We expect a return code of 0 because this is a properly formed properties set command.
-    if [[ "${rc}" != "0" ]]; then 
-        error "Failed to create property with name and value used."
-        return 1
-    fi
+}
+
+function get_stream {
+    local stream_name=$1
+    local bootstrap=$2
+    local output_file=$3
+    local format=$4
     
-    return 0
+    local cmd="${BINARY_LOCATION} streams get \
+    --name $stream_name \
+    --bootstrap $bootstrap \
+    --format $format \
+    --log -"
+
+    info "Getting stream with command: $cmd"
+
+    $cmd | tee "$output_file"
+    local rc=$?
+    
+    return $rc
 }
 
 #-----------------------------------------------------------------------------------------
 # Tests
 #----------------------------------------------------------------------------------------- 
+function streams_get_all {
+    h2 "Creating a test stream for get test"
+
+    set -o pipefail # Fail everything if anything in the pipeline fails. Else we are just checking the 'tee' return code.
+    
+    stream_name="mystream"
+
+    configure_stream "$stream_name" "$bootstrap"
+    rc=$?
+    
+    if [[ "${rc}" != "0" ]]; then 
+        error "Failed to configure stream properties with name $stream_name."
+        exit 1
+    fi
+
+    configure_stream "$stream_name_2" "$bootstrap"
+    rc=$?
+
+    if [[ "${rc}" != "0" ]]; then 
+        error "Failed to configure stream properties with name $stream_name_2."
+        exit 1
+    fi
+
+    # check that stream has been created
+    output_file="$ORIGINAL_DIR/temp/streams-get-output.txt"
+    local cmd="${BINARY_LOCATION} streams get \
+    --bootstrap $bootstrap \
+    --log -"
+
+    info "Fetching all streams"
+    $cmd
+    rc=$?
+
+    if [[ "${rc}" != "0" ]]; then 
+        error "Failed to get streams: command failed."
+        exit 1
+    fi
+
+    success "Fetched all streams"
+}
+
 function streams_create {
-    h2 "Performing properties set with name and value parameter used: create..."
+    h2 "Creating a test stream for get test"
 
     set -o pipefail # Fail everything if anything in the pipeline fails. Else we are just checking the 'tee' return code.
     
@@ -152,15 +210,10 @@ function streams_create {
     fi
 
     # check that stream has been created
-    cmd="${BINARY_LOCATION} streams get \
-    --name $stream_name \
-    --bootstrap $bootstrap \
-    --log -"
-
-    info "Command is: $cmd"
-
     output_file="$ORIGINAL_DIR/temp/streams-get-output.txt"
-    $cmd | tee $output_file
+    get_stream "$stream_name" "$bootstrap" "$output_file" "summary"
+    rc=$?
+
     if [[ "${rc}" != "0" ]]; then 
         error "Failed to get stream with name used: command failed."
         exit 1
@@ -172,23 +225,109 @@ function streams_create {
     rc=$?
     # We expect a return code of 0 because this is a properly formed streams get command.
     if [[ "${rc}" != "0" ]]; then 
-        error "Failed to create stream with name and value used."
+        error "Failed to get stream with name $stream_name."
         exit 1
     fi
 
     # Check that the previous properties set created a stream, with the correct values
-    cat $output_file | grep "Galasa" -q -E
-    cat $output_file | grep "mystream" -q -E
-    cat $output_file | grep "enabled" -q -E
+    grep -q -E "Galasa" "$output_file"
+    rc1=$?
 
-    rc=$?
-    # We expect a return code of 0 because this is a properly formed properties get command.
-    if [[ "${rc}" != "0" ]]; then 
-        error "Stream successfully created, but value incorrect."
+    grep -q -E "$stream_name" "$output_file"
+    rc2=$?
+
+    grep -q -E "enabled" "$output_file"
+    rc3=$?
+
+    # Check if any of the greps failed
+    if [[ "$rc1" != "0" || "$rc2" != "0" || "$rc3" != "0" ]]; then
+        # Report which specific check failed
+        [[ "$rc1" != "0" ]] && error "Missing 'Galasa' in stream output"
+        [[ "$rc2" != "0" ]] && error "Missing '$stream_name' in stream output"
+        [[ "$rc3" != "0" ]] && error "Missing 'enabled' in stream output"
+        error "Stream successfully created, but values incorrect."
         exit 1
     fi
 
     success "Stream set with name and value used seems to have been created correctly."
+}
+
+function streams_get_yaml_format {
+    h2 "Creating a test stream for get test"
+
+    set -o pipefail # Fail everything if anything in the pipeline fails. Else we are just checking the 'tee' return code.
+    
+    stream_name="mystream"
+    configure_stream "$stream_name" "$bootstrap"
+    rc=$?
+    
+    if [[ "${rc}" != "0" ]]; then 
+        error "Failed to configure stream properties."
+        exit 1
+    fi
+
+    # check that stream has been created
+    output_file="$ORIGINAL_DIR/temp/streams-get-output.txt"
+    get_stream "$stream_name" "$bootstrap" "$output_file" "yaml"
+    rc=$?
+
+    if [[ "${rc}" != "0" ]]; then 
+        error "Failed to get stream with name used: command failed."
+        exit 1
+    fi
+
+    # Check that the previous properties set created a stream property
+    cat $output_file | grep "$stream_name" -q
+
+    rc=$?
+    # We expect a return code of 0 because this is a properly formed streams get command.
+    if [[ "${rc}" != "0" ]]; then 
+        error "Failed to get stream with name $stream_name."
+        exit 1
+    fi
+
+    # Check that the previous properties set created a stream, with the correct values
+    grep -q -E "Galasa" "$output_file"
+    rc1=$?
+
+    grep -q -E "$stream_name" "$output_file"
+    rc2=$?
+
+    grep -q -E "GalasaStream" "$output_file"
+    rc3=$?
+
+    # Check if any of the greps failed
+    if [[ "$rc1" != "0" || "$rc2" != "0" || "$rc3" != "0" ]]; then
+        # Report which specific check failed
+        [[ "$rc1" != "0" ]] && error "Missing 'Galasa' in stream output"
+        [[ "$rc2" != "0" ]] && error "Missing '$stream_name' in stream output"
+        [[ "$rc3" != "0" ]] && error "Missing 'GalasaStream' in stream output"
+        error "Stream successfully created, but values incorrect."
+        exit 1
+    fi
+
+    success "Stream set with name and value used seems to have been created correctly and formatted in YAML"
+}
+
+function streams_get_missing_name {
+    h2 "Creating a test stream for get test"
+
+    set -o pipefail # Fail everything if anything in the pipeline fails. Else we are just checking the 'tee' return code.
+    
+    # check that stream has been created
+    output_file="$ORIGINAL_DIR/temp/streams-get-output.txt"
+    get_stream "some-unknown-stream" "$bootstrap" "$output_file" "summary"
+
+    if grep -qF \
+        'GAL5420E: Unable to retrieve a stream with the given stream name. No such stream exists.' \
+        "$output_file"
+    then
+        success "Stream with the name was missing and threw an error as expected."
+    else
+        error "Expected 404 error message not found."
+        exit 1
+    fi
+
 }
 
 function streams_delete {
@@ -221,25 +360,26 @@ function streams_delete {
     fi
 
     # check that stream has been deleted
-    cmd="${BINARY_LOCATION} streams get \
-    --name $stream_name \
-    --bootstrap $bootstrap \
-    --log -"
-
-    info "Command is: $cmd"
-
     output_file="$ORIGINAL_DIR/temp/streams-get-output.txt"
-    $cmd | tee $output_file
+    get_stream "$stream_name" "$bootstrap" "$output_file" "summary"
 
-    # Check for successful deletion, it should return 404 response code
-    cat $output_file | grep "GAL5420E: Unable to retrieve a stream with the given stream name. No such stream exists." -q -E
-    
-
-    success "Properties delete with name used seems to have been deleted correctly."
+    if grep -qF \
+        'GAL5420E: Unable to retrieve a stream with the given stream name. No such stream exists.' \
+        "$output_file"
+    then
+        success "Stream with the name was deleted correctly."
+    else
+        error "Deletion verification failed: expected 404 error message not found."
+        exit 1
+    fi
+        
 }
 
 function streams_tests {
     streams_create
+    streams_get_yaml_format
+    streams_get_missing_name
+    streams_get_all
     streams_delete
 }
 
